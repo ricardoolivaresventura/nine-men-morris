acf18575f75697064351e19ed0958416593d8c54
ricardoolivaresventura <ricardo.olivares.v@uni.pe>
2022-10-13 00:20:21 -0500

    Set circle buttons dynamicaly

--- src/main/java/com/example/ninemenmorris/WindowTwoController.java
+++ src/main/java/com/example/ninemenmorris/WindowTwoController.java
@@ -7,32 +7,43 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class WindowTwoController implements Initializable {
     @FXML
     Button btn1;
+    @FXML
+    Pane mainContainer;
 
-    public void setBtnTitle(){
-        btn1.setStyle(
-                "-fx-background-radius: 5em; " +
-                        "-fx-min-width: 30px; " +
-                        "-fx-min-height: 30px; " +
-                        "-fx-max-width: 30px; " +
-                        "-fx-max-height: 30px; " +
-                        "-fx-background-color: black;" +
-                        "-fx-background-insets: 0px; " +
-                        "-fx-padding: 0px;"
-        );
-        btn1.setText("");
+    List<Button> buttonList = new ArrayList();
+    List containerChildren = new ArrayList();
+
+    String buttonStyle = "-fx-background-radius: 5em; " +
+            "-fx-min-width: 30px; " +
+            "-fx-min-height: 30px; " +
+            "-fx-max-width: 30px; " +
+            "-fx-max-height: 30px; " +
+            "-fx-background-color: black;" +
+            "-fx-background-insets: 0px; " +
+            "-fx-padding: 0px;";
+
+    public void setBtnStyle(Button btn){
+        btn.setStyle(buttonStyle);
     }
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setBtnTitle();
+        containerChildren = mainContainer.getChildren();
+        for(int i = 0; i < containerChildren.size(); i++){
+            if(containerChildren.get(i) instanceof Button){
+                setBtnStyle((Button) containerChildren.get(i));
+            }
+        }
     }
 }
