3bcc25457343a303b45a017be9158d9d0de9369e
ricardoolivaresventura <ricardo.olivares.v@uni.pe>
2022-10-23 11:16:29 -0500

    Add methods and attributes in PlayerFile class

--- src/main/java/classes/PlayerFile.java
+++ src/main/java/classes/PlayerFile.java
@@ -1,9 +1,42 @@
 package classes;
 
 public class PlayerFile {
-    public PlayerFile() {
+    private char color;
+    private int row, column;
+    public PlayerFile(char color, int row, int column) {
+        this.color = color;
+        this.row = row;
+        this.column = column;
     }
     public void showMessage(){
         System.out.println("MOSTRAR MENSAJE");
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public void setColor(char color) {
+        this.color = color;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public String getPositions (){
+        return "Fila: "+ this.row + ", Columna: " + this.column;
     }
 }
