10e9a5a238fe1a0902d70bec858c4ae7f442cf50
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-10-23 19:32:32 -0500

    add test array

--- src/main/java/textMill/testMill.java
+++ src/main/java/textMill/testMill.java
@@ -1,4 +1,15 @@
 package textMill;
 
 public class testMill {
+    public static void main(String[] args) {
+        char [][] tablero = {
+                {'C','X','X','C','X','X','C'},
+                {'X','C','X','C','X','B','X'},
+                {'X','X','C','C','C','X','X'},
+                {'C','B','C','X','C','B','B'},
+                {'X','X','C','C','C','X','X'},
+                {'X','C','X','C','X','B','X'},
+                {'C','X','X','C','X','X','C'}
+        };
+    }
 }
