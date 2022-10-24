96b471604558928d2b78ee3e7ad99c7258bf3711
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-10-23 19:51:32 -0500

    add method horizontalMill()

--- src/main/java/textMill/testMill.java
+++ src/main/java/textMill/testMill.java
@@ -2,14 +2,34 @@ package textMill;
 
 public class testMill {
     public static void main(String[] args) {
-        char [][] tablero = {
+        char [][] board = {
                 {'C','X','X','C','X','X','C'},
-                {'X','C','X','C','X','B','X'},
+                {'X','B','X','B','X','B','X'},
                 {'X','X','C','C','C','X','X'},
-                {'C','B','C','X','C','B','B'},
+                {'C','C','C','X','C','C','C'},
                 {'X','X','C','C','C','X','X'},
-                {'X','C','X','C','X','B','X'},
+                {'X','C','X','C','X','C','X'},
                 {'C','X','X','C','X','X','C'}
         };
+        int rows = 1;
+        int columns = 1;
+
+        System.out.println(horizontalMill(board,rows,columns));
+    }
+    /*static Boolean mill(char[][] board,int rows,int columns){
+
+    }*/
+    static Boolean horizontalMill(char[][] board,int rows,int columns){
+        int cont = 0;
+        //search in rows
+        for(int i=0;i<7;i++){
+            if(board[rows][i]=='B'){
+                cont++;
+            }
+        }
+        if(cont==3){
+            return true;
+        }
+        return false;
     }
 }
