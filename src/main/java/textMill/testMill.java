0696cd3b6c51a60230f9217c1ffafc7ac9744774
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-10-23 21:17:27 -0500

    add method specialMill() - case row three

--- src/main/java/textMill/testMill.java
+++ src/main/java/textMill/testMill.java
@@ -4,17 +4,17 @@ public class testMill {
     public static void main(String[] args) {
         char [][] board = {
                 {'C','X','X','C','X','X','C'},
-                {'X','B','X','C','X','C','X'},
+                {'X','C','X','C','X','C','X'},
                 {'X','X','C','C','C','X','X'},
-                {'C','B','C','X','C','C','C'},
+                {'B','B','B','X','C','C','C'},
                 {'X','X','C','C','C','X','X'},
-                {'X','B','X','C','X','C','X'},
+                {'X','C','X','C','X','C','X'},
                 {'C','X','X','C','X','X','C'}
         };
-        int rows = 1;
-        int columns = 1;
+        int rows = 3;
+        int columns = 0;
 
-        System.out.println(verticalMill(board,rows,columns));
+        System.out.println(specialMill(board,rows,columns));
     }
     /*static Boolean mill(char[][] board,int rows,int columns){
 
@@ -42,6 +42,18 @@ public class testMill {
         }
         if(cont==3){
             return true;
+        }
+        return false;
+    }
+    static Boolean specialMill(char[][] board, int rows,int columns){
+        if(rows==3){
+            if(board[rows][0]=='B' && board[rows][1]=='B' && board[rows][2]=='B'){
+                return true;
+            }else if(board[rows][4]=='B' && board[rows][5]=='B' && board[rows][6]=='B'){
+                return true;
+            }else{
+                return false;
+            }
         }
         return false;
     }
