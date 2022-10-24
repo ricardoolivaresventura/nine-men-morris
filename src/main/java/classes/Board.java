82d3c0189302626ce1e77afb8b39c0b9a578293c
BreinerCatalinoMorales <breiner.catalino.m@uni.pe>
2022-10-23 19:14:02 -0500

    initializing the board class

--- src/main/java/classes/Board.java
+++ src/main/java/classes/Board.java
@@ -1,13 +1,20 @@
 package classes;
 
 public class Board {
-    private PlayerFile files[][];
-    public Board(){
-        this.files = new PlayerFile[GlobalConstants.ROWS][GlobalConstants.COLUMN];
+    private PlayerFile[][] files;
+
+    public Board() {
+        files = new PlayerFile[GlobalConstants.ROWS][GlobalConstants.COLUMN];
+        for (int i = 0; i < GlobalConstants.ROWS; i++) {
+            for (int j = 0; j < GlobalConstants.COLUMN; j++) {
+                files[i][j] = PlayerFile.fil_null();
+            }
+        }
     }
 
-    public void setFilePosition(){
+
+    public static void main(String[] args) {
+        Board b = new Board();
 
     }
-
 }
