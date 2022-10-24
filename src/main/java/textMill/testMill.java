fbd0ebfbd7452355ac3b5c2a4487a3c922494c36
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-10-23 19:56:13 -0500

    add method verticalMill()

--- src/main/java/textMill/testMill.java
+++ src/main/java/textMill/testMill.java
@@ -4,17 +4,17 @@ public class testMill {
     public static void main(String[] args) {
         char [][] board = {
                 {'C','X','X','C','X','X','C'},
-                {'X','B','X','B','X','B','X'},
+                {'X','B','X','C','X','C','X'},
                 {'X','X','C','C','C','X','X'},
-                {'C','C','C','X','C','C','C'},
+                {'C','B','C','X','C','C','C'},
                 {'X','X','C','C','C','X','X'},
-                {'X','C','X','C','X','C','X'},
+                {'X','B','X','C','X','C','X'},
                 {'C','X','X','C','X','X','C'}
         };
         int rows = 1;
         int columns = 1;
 
-        System.out.println(horizontalMill(board,rows,columns));
+        System.out.println(verticalMill(board,rows,columns));
     }
     /*static Boolean mill(char[][] board,int rows,int columns){
 
@@ -24,6 +24,19 @@ public class testMill {
         //search in rows
         for(int i=0;i<7;i++){
             if(board[rows][i]=='B'){
+                cont++;
+            }
+        }
+        if(cont==3){
+            return true;
+        }
+        return false;
+    }
+    static Boolean verticalMill(char[][] board, int rows,int columns){
+        int cont = 0;
+        //search in columns
+        for(int i=0;i<7;i++){
+            if(board[i][columns]=='B'){
                 cont++;
             }
         }
