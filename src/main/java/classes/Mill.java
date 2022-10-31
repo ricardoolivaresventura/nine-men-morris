b7646f72ee6d329ee30cb11e2eb0d3478cc979f7
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-10-30 22:08:51 -0500

    copy content from testMill class to Mill

--- src/main/java/classes/Mill.java
+++ src/main/java/classes/Mill.java
@@ -11,5 +11,59 @@ public class Mill {
         this.column = column;
     }
 
+    Boolean mill(char[][] board,int rows,int columns){
+        if(rows==3 || columns == 3){
+            return specialMill(board,  rows,  columns);
+        }
+        return horizontalMill(board,rows,columns);
+    }
 
+    Boolean horizontalMill(char[][] board,int rows,int columns){
+        int cont = 0;
+        //search in rows
+        for(int i=0;i<7;i++){
+            if(board[rows][i]=='B'){
+                cont++;
+            }
+        }
+        if(cont==3){
+            return true;
+        }
+        return verticalMill(board,rows,columns);
+    }
+
+    Boolean verticalMill(char[][] board, int rows,int columns){
+        int cont = 0;
+        //search in columns
+        for(int i=0;i<7;i++){
+            if(board[i][columns]=='B'){
+                cont++;
+            }
+        }
+        if(cont==3){
+            return true;
+        }
+        return false;
+    }
+    Boolean specialMill(char[][] board, int rows,int columns){
+        if(rows == 3){
+            if(board[rows][0]=='B' && board[rows][1]=='B' && board[rows][2]=='B'){
+                return true;
+            }else if(board[rows][4]=='B' && board[rows][5]=='B' && board[rows][6]=='B'){
+                return true;
+            }else{
+                return verticalMill(board,  rows,  columns);
+            }
+        }
+        if(columns == 3){
+            if(board[0][columns]=='B' && board[1][columns]=='B' && board[2][columns]=='B'){
+                return true;
+            }else if(board[4][columns]=='B' && board[5][columns]=='B' && board[6][columns]=='B'){
+                return true;
+            }else{
+                return horizontalMill(board,  rows,  columns);
+            }
+        }
+        return false;
+    }
 }
