47f1581bcc73e27680e88960234f1ce4ba1820a3
LuisBacaSandoval <luis.baca.s@uni.pe>
2022-11-02 11:37:17 -0500

    create MillPageController class

---
+++ src/main/java/com/example/ninemenmorris/MillPageController.java
@@ -0,0 +1,13 @@
+package com.example.ninemenmorris;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MillPageController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
