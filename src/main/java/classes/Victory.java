8cac0b460405f9a016deb43b0b08674c314da9af
BreinerCatalinoMorales <breiner.catalino.m@uni.pe>
2022-11-03 14:22:59 -0500

    completing victory class

--- src/main/java/classes/Victory.java
+++ src/main/java/classes/Victory.java
@@ -2,6 +2,23 @@ package classes;
 
 public class Victory {
     public static void victory(PlayerFile[][] board){
-
+        int blackPlayer=0;
+        int redPlayer=0;
+        for(int i=0;i<GlobalConstants.ROWS;i++){
+            for(int j=0;j<GlobalConstants.COLUMN;j++){
+                if (board[i][j].getColor()==GlobalConstants.BLACK){
+                    blackPlayer++;
+                }
+                if (board[i][j].getColor()==GlobalConstants.RED){
+                    redPlayer++;
+                }
+            }
+        }
+        if(redPlayer<3){
+            System.out.println("Victoria para el jugador negro");
+        }
+        if (blackPlayer<3){
+            System.out.println("Victoria para el jugador rojo");
+        }
     }
 }
